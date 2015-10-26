1ef3882214e95cf2a3b8d9bf009e1477581fd372
Nik Nyby <nnyby@columbia.edu>
2015-10-26 13:08:44 -0400

    add jshint

--- test/test-menuitems.js
+++ test/test-menuitems.js
@@ -1,14 +1,14 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-'use strict'
+'use strict';
 
 const windowUtils = require('sdk/deprecated/window-utils');
 const menuitems = require('../index');
 
 let window = windowUtils.activeBrowserWindow;
 let document = window.document;
-function $(id) document.getElementById(id);
+function $(id) document.getElementById(id)
 
 function createMI(options, test) {
   test.equal(!$(options.id), true);
